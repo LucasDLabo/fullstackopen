bcd02217f4c37d9d641a8e00707bdf310b623b63
Lucas Del Labo <121238683+LucasDLabo@users.noreply.github.com>
2025-10-13 20:51:38 -0300

    [part2] - "2.2: Course information step 7" Completed

--- part2/courseInformation/project/src/components/Content.jsx
+++ part2/courseInformation/project/src/components/Content.jsx
@@ -2,13 +2,17 @@ import Part from './Part';
 
 const Content = ({parts}) => {
     return (
-        <ul>
-            {parts.map(part => 
-                <li key={part.id}>
-                    <Part name={part.name} exercises={part.exercises} />
-                </li>
-            )}
-        </ul>
+        <>
+            <ul>
+                {parts.map(part => 
+                    <li key={part.id}>
+                        <Part name={part.name} exercises={part.exercises} />
+                    </li>
+                )}
+            </ul>
+            
+            <p>total of {parts.reduce((acum, part) => acum + part.exercises, 0)} exercises</p>
+        </>
     );
 }
 
